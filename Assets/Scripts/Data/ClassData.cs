ddf6a4ceaeeea64880386ad3adf8ae267c005c15
dylanbernard <dylanbernard@hotmail.fr>
2025-10-21 15:34:10 +0900

    Add - ClassData logic

--- Assets/Scripts/Data/ClassData.cs
+++ Assets/Scripts/Data/ClassData.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 
-public class ClassData : MonoBehaviour
+[CreateAssetMenu(menuName = "GodVeil/Class Data")]
+public class ClassData : ScriptableObject
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Base Info")]
+    public string className;
+    public Sprite classIcon;
+    public GameObject classObject;
+    public RuntimeAnimatorController animator;
+    public RankType rank;
+    public bool isLocked = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Base Stats")]
+    public StatProfile baseStats; // Stock stats FOR/DEX/INT....
+
+    [Header("Links")]
+    public SkillData autoAttack;
+    public SkillData specialSkill;
+    public SkillData ultimateSkill;
+    public PassiveData passive;
 }
+
+public enum StatType { Strength, Dexterity, Intelligence, Endurance, Magic }
+public enum SkillType { AutoAttack, Special, Ultimate }
+public enum ElementType { None, Fire, Water, Earth, Lightning, Poison }
+public enum RankType { Rank0, Rank1, Rank2 }
