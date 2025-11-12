7fa9b6203de6fddb27e589608680afbeb2e6740f
マックス <maxoumixou@gmail.com>
2025-11-12 13:48:54 +0900

    (add)
    - Added an effect on click (to know where player is clicking)

--- Assets/Scripts/UI/CursorTrail.cs
+++ Assets/Scripts/UI/CursorTrail.cs
@@ -4,11 +4,18 @@ public class CursorTrail : MonoBehaviour
 {
 
     [SerializeField] private Transform mouseEffectPos;
+    [SerializeField] private ParticleSystem clickEffectPos;
 
     // Update is called once per frame
     void Update()
     {
         Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouseEffectPos.position = cursorPos;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            clickEffectPos.transform.position = cursorPos;
+            clickEffectPos.Play();
+        }
     }
 }
